ba89c2f8b40fb8e8b7a81ee1538a3d6859856740
dikshaturkar2022-sys <dikshaturkar2022@gmail.com>
2026-04-28 09:26:44 +0530

    terraform files corrected

--- terraform/envs/dev/providers.tf
+++ terraform/envs/dev/providers.tf
@@ -2,27 +2,3 @@ provider "aws" {
   region = var.region
 }
 
-# Get cluster details AFTER creation
-data "aws_eks_cluster" "cluster" {
-  name = module.eks.cluster_name
-}
-
-data "aws_eks_cluster_auth" "cluster" {
-  name = module.eks.cluster_name
-}
-
-# Kubernetes provider
-provider "kubernetes" {
-  host                   = data.aws_eks_cluster.cluster.endpoint
-  cluster_ca_certificate = base64decode(data.aws_eks_cluster.cluster.certificate_authority[0].data)
-  token                  = data.aws_eks_cluster_auth.cluster.token
-}
-
-# Helm provider (IMPORTANT: kubernetes block MUST be inside this)
-provider "helm" {
-    kubernetes {
-        host                   = data.aws_eks_cluster.cluster.endpoint
-        cluster_ca_certificate = base64decode(data.aws_eks_cluster.cluster.certificate_authority[0].data)
-        token                  = data.aws_eks_cluster_auth.cluster.token
-    }
-}
